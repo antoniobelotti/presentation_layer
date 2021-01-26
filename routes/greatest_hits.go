9c7ddeb7aae791dd31e85db1c009cd5f8e7cab38
antonio <antonio.belotti@studenti.unimi.it>
2021-01-26 18:49:55 +0100

    front-end: add greatest hits basic content

--- routes/greatest_hits.go
+++ routes/greatest_hits.go
@@ -3,17 +3,22 @@ package routes
 import (
 	"html/template"
 	"net/http"
+	"web/main/models"
 )
 
-type GreatestHitsData struct {
-	Title string
-	Message string
-}
 
 func GreatestHitsHandler(w http.ResponseWriter, r *http.Request) {
-	t := template.Must(template.ParseFiles("templates/base.html", "templates/greatest_hits/index.html"))
-	t.Execute(w, GreatestHitsData{
-		Title: "",
-		Message: "",
-	})
+	rows, err := models.TodayGreatestHits()
+	if err != nil {
+		// return error page
+	}
+
+	funcMap := template.FuncMap{
+		// The name "inc" is what the function will be called in the template text.
+		"inc": func(i int) int {
+			return i + 1
+		},
+	}
+	t := template.Must(template.New("base.html").Funcs(funcMap).ParseFiles("templates/base.html", "templates/greatest_hits/index.html"))
+	t.Execute(w, rows)
 }
--- routes/playlists.go
+++ routes/playlists.go
@@ -5,15 +5,7 @@ import (
 	"net/http"
 )
 
-type PlaylistsData struct {
-	Title string
-	Message string
-}
-
 func PlaylistsHandler(w http.ResponseWriter, r *http.Request) {
 	t := template.Must(template.ParseFiles("templates/base.html", "templates/playlists/index.html"))
-	t.Execute(w, GreatestHitsData{
-		Title: "",
-		Message: "",
-	})
+	t.Execute(w, nil)
 }
